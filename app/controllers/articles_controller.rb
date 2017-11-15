91bf78914a35067dafcdcbff27501e9daf4b03bf
Ryota Suwa <r.s.30402457.m.p@gmail.com>
2017-11-15 07:29:09 +0000

    Fix some bags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,33 @@
 class ArticlesController < ApplicationController
-    before_action :set_ariticle
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @article = Article.all
     end
+
     def show
-        # @article = Ariticle.find(params[:id])
+        @comment = Comment.new
+        @comments = @article.comments.includes(:article)
     end
+
     def new
         @article = Article.new
     end
+
     def create
         @article = Article.new article_params
         if @article.save
-            redirect_to :root
             flash[:success] = "作成に成功しました"
+            redirect_to :root
         else
             render :new
         end
     end
+
     def edit
-        @article = Ariticle.find(params[:id])
     end
+
     def update
-        @article = Ariticle.find(params[:id])
         if @article.update_attributes article_params
             flash[:success] = "更新に成功しました"
             redirect_to :root
@@ -31,6 +36,7 @@ class ArticlesController < ApplicationController
         end
             
     end
+
     def destroy
         if @article.destroy
             flash[:success] = "削除に成功しました"
@@ -41,15 +47,15 @@ class ArticlesController < ApplicationController
     end
     
     private
-    def set_ariticle
-         @article = Ariticle.find(params[:id])
+    def set_article
+         @article = Article.find(params[:id])
     end
     
     def article_params
-        params.require(:aricle).permit(
+        params.require(:article).permit(
             :name,
             :title,
             :content
-            )
+        )
     end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,47 @@
 class CommentsController < ApplicationController
+    before_action :set_article
+    before_action :set_comment, only: [:update, :destroy]
+    
+    def update
+        if @comment.update_attributes comment_params
+            flash[:success] = "更新に成功しました"
+        else
+            flash[:error] = "更新に失敗しました"
+        end
+        redirect_to @article
+    end
+    
+    def create
+        if @article.comments.create comment_params
+            flash[:success] = "コメントしました"
+        else
+            flash[:error] = "コメントに失敗しました"
+        end
+        redirect_to @article
+    end
+    
+    def destroy
+        if @comment.destroy
+            flash[:success] = "コメントを削除しました"
+        else
+            flash[:error] = "コメントの削除に失敗しました"
+        end
+        redirect_to @article
+    end
+    
+    private
+        def comment_params
+            params.require(:comment).permit(
+                :from,
+                :body
+            )
+        end
+        
+        def set_article
+            @article = Article.find(params[:article_id])
+        end
+        
+        def set_comment
+            @comment = Comment.find(params[:id])
+        end
 end
